24d4b4a5b6c14d09af4a83ebc61dc3cd251c74ea
Esha Patel <Patellesha11@gmail.com>
2025-04-15 20:10:49 +0300

    added

--- src/Components/Image.js
+++ src/Components/Image.js
@@ -4,5 +4,6 @@ import './Image.css';
 const images = [
     'https://source.unsplash.com/600x300/?paris,eiffel-tower',
     'https://source.unsplash.com/600x300/?new-york,city',
+    'https://source.unsplash.com/600x300/?tokyo,cityscape',
 
     ]
